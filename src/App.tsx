abf8079e013bc146a2bb6cd976b7b0be6e07c00c
chanchana <north1602@gmail.com>
2021-08-14 12:27:38 +0700

    Add url

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import { Home } from './pages/Home'
+import { Pomodoro } from './pages/Pomodoro'
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" component={Home} />
+      </Switch>
+      <Switch>
+        <Route path="/pomodoro" component={Pomodoro} />
       </Switch>
     </BrowserRouter>
   )
