ce74bbee0f7bff59a4b6e77b7af986ba4f37c707
Suphon Thanakornpakapong <paphonb@gmail.com>
2021-08-14 17:38:23 +0700

    Fix landing page link

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,13 +1,14 @@
-import React, { ReactElement } from "react";
-import styled from '@emotion/styled';
-import NavLogo from '@/assets/images/logo.svg';
-import ButtonLogo from '@/assets/images/logo_button.svg';
-import TaglineLogo from '@/assets/images/logo_tagline.svg';
-import LandingHero from '@/assets/images/landing_hero.svg';
-import Triangle from '@/assets/images/purple_triangle.svg';
-import DescriptionArt from '@/assets/images/desc_art.svg';
-import { Box, Button, Typography } from '@material-ui/core';
-import { useHistory } from 'react-router-dom'
+import styled from '@emotion/styled'
+import { Box, Button, Typography } from '@material-ui/core'
+import React, { ReactElement } from 'react'
+import { Link } from 'react-router-dom'
+
+import DescriptionArt from '@/assets/images/desc_art.svg'
+import LandingHero from '@/assets/images/landing_hero.svg'
+import NavLogo from '@/assets/images/logo.svg'
+import ButtonLogo from '@/assets/images/logo_button.svg'
+import TaglineLogo from '@/assets/images/logo_tagline.svg'
+import Triangle from '@/assets/images/purple_triangle.svg'
 
 const Page = styled.div`
   // background: red;
@@ -16,13 +17,12 @@ const Page = styled.div`
   width: 100%;
 `
 
-const description = '“Wetime” ได้แนวคิดมาจากคำว่า We ที่หมายถึง พวกเรา และ Time ที่หมายถึง เวลา ที่นำมารวมกันแล้วคือ การเข้ามาใช้เวลาร่วมกัน การทำงานร่วมกันมันเป็นอะไรที่น่าสนุกนะ แต่บางครั้งการทำงานร่วมกันก็กินเวลาจนหาข้อสรุปไม่ได้ Wetime จึงมีไอเดียอยากให้การประชุมไม่น่าเบื่อและจำเจ เอาหลักการแบ่งเวลามาปรับใช้ให้ทุกคน Produdctive มากขึ้น และยิ่งเป็นยุค New Normal แบบนี้ ถ้าแบ่งเวลาไม่ดี ยิ่งน่าปวดหัว'
-const description2 = 'Wetime อยากช่วยให้ทุกคน Productive และทำงานง่าย ๆ เพราะแค่ทำงานที่บ้านก็เซ็งมากพอแล้ว อย่าให้การแบ่งเวลามาเป็นปัญหาของทุกคน พวกเราเองก็หวังว่าทุกคนจะทำงานได้อย่างราบรื่นจากเว็บไซด์ดี ๆ อย่าง Wetime ของเรานะ!'
+const description =
+  '“Wetime” ได้แนวคิดมาจากคำว่า We ที่หมายถึง พวกเรา และ Time ที่หมายถึง เวลา ที่นำมารวมกันแล้วคือ การเข้ามาใช้เวลาร่วมกัน การทำงานร่วมกันมันเป็นอะไรที่น่าสนุกนะ แต่บางครั้งการทำงานร่วมกันก็กินเวลาจนหาข้อสรุปไม่ได้ Wetime จึงมีไอเดียอยากให้การประชุมไม่น่าเบื่อและจำเจ เอาหลักการแบ่งเวลามาปรับใช้ให้ทุกคน Produdctive มากขึ้น และยิ่งเป็นยุค New Normal แบบนี้ ถ้าแบ่งเวลาไม่ดี ยิ่งน่าปวดหัว'
+const description2 =
+  'Wetime อยากช่วยให้ทุกคน Productive และทำงานง่าย ๆ เพราะแค่ทำงานที่บ้านก็เซ็งมากพอแล้ว อย่าให้การแบ่งเวลามาเป็นปัญหาของทุกคน พวกเราเองก็หวังว่าทุกคนจะทำงานได้อย่างราบรื่นจากเว็บไซด์ดี ๆ อย่าง Wetime ของเรานะ!'
 
 export function Landing(): ReactElement {
-
-  const history = useHistory();
-
   const Nav = (
     <Box height="64px" display="flex" padding="48px 64px" color="#555555">
       <Box>
@@ -46,14 +46,29 @@ export function Landing(): ReactElement {
   const Hero = (
     <Box padding="24px 64px" height="460px" position="relative">
       <Box width="540px">
-        <Typography fontSize="48px" fontWeight={500} color="primary" lineHeight="54px">ใช้เวลาของคุณอย่างคุ้มค่า ไปกับ วีไทม์..</Typography>
+        <Typography
+          fontSize="48px"
+          fontWeight={500}
+          color="primary"
+          lineHeight="54px"
+        >
+          ใช้เวลาของคุณอย่างคุ้มค่า ไปกับ วีไทม์..
+        </Typography>
       </Box>
       <Box width="400px" color="#555555" marginTop="16px" marginBottom="32px">
-        <Typography fontSize="16px" fontWeight={300} lineHeight="24px">การทำงานร่วมกันมันเป็นอะไรที่น่าสนุกนะ แต่บางครั้งการทำงานร่วมกันก็กินเวลาจนหาข้อสรุปไม่ได้ Wetime จึงมีไอเดียอยากให้การประชุมไม่น่าเบื่อและจำเจ</Typography>
+        <Typography fontSize="16px" fontWeight={300} lineHeight="24px">
+          การทำงานร่วมกันมันเป็นอะไรที่น่าสนุกนะ
+          แต่บางครั้งการทำงานร่วมกันก็กินเวลาจนหาข้อสรุปไม่ได้ Wetime
+          จึงมีไอเดียอยากให้การประชุมไม่น่าเบื่อและจำเจ
+        </Typography>
       </Box>
-      <Button onClick={() => { history.push('/home') }} variant="contained">เริ่มใช้<img src={ButtonLogo} style={{margin: '0 8px'}} />บนบราวเซอร์คุณ</Button>
+      <Button component={Link} to="/app" variant="contained">
+        เริ่มใช้
+        <img src={ButtonLogo} style={{ margin: '0 8px' }} />
+        บนบราวเซอร์คุณ
+      </Button>
       <Box position="absolute" right="48px" bottom="0">
-        <img src={LandingHero} style={{width: '620px'}}/>
+        <img src={LandingHero} style={{ width: '620px' }} />
       </Box>
     </Box>
   )
@@ -61,9 +76,15 @@ export function Landing(): ReactElement {
   const Description = (
     <Box display="flex" marginTop="200px" flexDirection="column">
       <Box width="325px" margin="auto">
-        <img src={TaglineLogo}/>
+        <img src={TaglineLogo} />
       </Box>
-      <Box width="570px" margin="auto" marginTop="46px" color="#555555" textAlign="center">
+      <Box
+        width="570px"
+        margin="auto"
+        marginTop="46px"
+        color="#555555"
+        textAlign="center"
+      >
         <Typography fontWeight={300}>{description}</Typography>
       </Box>
     </Box>
@@ -72,9 +93,9 @@ export function Landing(): ReactElement {
   const Description2 = (
     <Box position="absolute" left="0" width="100%">
       <Box zIndex={-100}>
-        <img src={Triangle} style={{width: '100%'}}/>
+        <img src={Triangle} style={{ width: '100%' }} />
       </Box>
-      <Box style={{background: '#4A2EF4'}} marginTop="-40px" zIndex={0}>
+      <Box style={{ background: '#4A2EF4' }} marginTop="-40px" zIndex={0}>
         <Box width="100%" display="flex">
           <Box margin="auto">
             <Box margin="-50px -34px 0 0">
